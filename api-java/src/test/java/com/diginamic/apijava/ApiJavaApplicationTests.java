f7328ee3e74075ea4f295ac66c5eb2dbc570861f
Otmane Boujlam <otmane.boujlam@gmail.com>
2024-03-21 14:57:43 +0100

    first commit

---
+++ api-java/src/test/java/com/diginamic/apijava/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.apijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
